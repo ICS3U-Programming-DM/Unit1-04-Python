c0cb47b20c7b982fa98e8ace3e568a1f3e0bd3a2
Ubuntu <ubuntu@ip-172-31-17-200.ec2.internal>
2021-11-25 19:21:41 +0000

    created area of a rectangle

---
+++ area_per_rect.py
@@ -0,0 +1,16 @@
+# created by Dylan Melo
+# created on November 2021
+# this program calculates the area and perimiter of a rectangle
+
+
+def main():
+    # this function calculates the area and perimiter
+    print("If a rectangle has the dimensions:")
+    print("Length 5cm x 9cm")
+    print("")
+    print("Area is {}cm^2".format(5*9))
+    print("Perimiter is {}cm".format(2*(5+9)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,8 @@
+# created by Dylan Melo
+# created on November 24th
+# this program does basic math
+
+
+print("12 / 3 = {}". format(12/3))
+print("10 - 4 / 9 = {}". format(10-4/9))
+print("10^10 = {}". format(10**10))
